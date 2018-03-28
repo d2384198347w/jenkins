a3fcbf46e88a3cc84e3242834b0797c8e191abbf
Jenkinsadmin <localhost>
2018-03-28 03:14:07 -0400

    add

--- auto_write.sh
+++ auto_write.sh
@@ -2,3 +2,12 @@
 echo "It is " $(date) "now, and write time to ./date.txt"
 echo $(pwd)
 date > ./jenkins/date.txt
+
+echo "#change dir to github jenkins"
+cd jenkins/
+echo "#git pull latest code"
+git pull
+echo "#git commit changes"
+git commit -am "add"
+echo "#git push to master"
+git push origin master 
